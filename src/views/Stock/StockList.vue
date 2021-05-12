b3b3c73ec3e66dd25fcc410d2b103e8a59cd5f8b
马超 <1162714483@qq.com>
2021-05-12 20:16:25 +0800

    修改socket地址

--- src/views/Stock/StockList.vue
+++ src/views/Stock/StockList.vue
@@ -270,9 +270,9 @@ export default {
       var connectUrl = ''
 
       if (window.location.protocol == 'http:') {
-        connectUrl = 'ws://localhost:8764/websocket/' + 'stock'
+        connectUrl = 'ws://121.89.222.103:8764/websocket/' + 'stock'
       } else {
-        connectUrl = 'wss://localhost:8764/websocket/' + 'stock'
+        connectUrl = 'wss://121.89.222.103:8764/websocket/' + 'stock'
       }
 
       if ('WebSocket' in window) {
